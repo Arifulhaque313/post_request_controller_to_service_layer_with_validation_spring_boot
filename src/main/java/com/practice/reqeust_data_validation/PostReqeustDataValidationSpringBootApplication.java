907818025b20517877582cdd00cfa7ed06f2a4e1
arifulhaque313 <asajib7654@gmail.com>
2024-09-24 21:42:45 +0600

    :sparkles: init*

---
+++ src/main/java/com/practice/reqeust_data_validation/PostReqeustDataValidationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.practice.reqeust_data_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostReqeustDataValidationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostReqeustDataValidationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/reqeust_data_validation/PostReqeustDataValidationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.reqeust_data_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostReqeustDataValidationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
